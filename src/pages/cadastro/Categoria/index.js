3861084e9f6bfb70878d938db08a8557e77d3104
Bruno Alcantara <brunnoalcantara97@gmail.com>
2020-07-30 21:16:38 -0300

    Test deploy backend

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -33,19 +33,16 @@ function CadastroCategoria() {
   // }
 
   useEffect(() => {
-    if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
-      fetch(URL)
-       .then(async (respostaDoServer) =>{
-        if(respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          setCategorias(resposta);
-          return; 
-        }
-        throw new Error('Não foi possível pegar os dados');
-       })
-    }    
-  }, []);
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://primeflixbr.herokuapp.com/categorias';
+    fetch(URL)
+      .then(async(respostaDoServidor) =>{
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
 
   return (
     <PageDefault>
